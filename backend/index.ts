d65130c787dd84e8c7c14f6c5ce7657c4a760475
Luca1905 <contact@lucawang.me>
2025-05-17 14:27:30 +0200

    Added biome to backend

--- backend/index.ts
+++ backend/index.ts
@@ -3,30 +3,36 @@ import { Redis } from "@upstash/redis";
 const redis = Redis.fromEnv();
 
 async function incrementCount() {
-  const count = await redis.incr("count");
-  return new Response(JSON.stringify({ count }), {
-    status: 200,
-    headers: { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*'},
-  });
+	const count = await redis.incr("count");
+	return new Response(JSON.stringify({ count }), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+	});
 }
 
 async function getCount() {
-  const count = (await redis.get("count")) ?? 0;
-  return new Response(JSON.stringify({ count }), {
-    status: 200,
-    headers: { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*'},
-  });
+	const count = (await redis.get("count")) ?? 0;
+	return new Response(JSON.stringify({ count }), {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+	});
 }
 
 const server = Bun.serve({
-  routes: {
-    "/api/count/increment": incrementCount,
-    "/api/count": getCount,
-  },
+	routes: {
+		"/api/count/increment": incrementCount,
+		"/api/count": getCount,
+	},
 
-  fetch(_req) {
-    return new Response("Not Found", { status: 404 });
-  },
+	fetch(_req) {
+		return new Response("Not Found", { status: 404 });
+	},
 });
 
 console.log("Backend API on port:", server.port);
